35b78b716b3b3c5094bf960fa9fca6f4fbe0288e
yatsev1ch <ayatsevdeveloper@gmail.com>
2021-08-25 03:14:34 +0300

    Разметка главной

--- source/js/scripts.js
+++ source/js/scripts.js
@@ -1,12 +1,23 @@
 const menuToggle = document.querySelector(".page-header__menu-toggle");
 const menuContainer = document.querySelector(".page-header__main-menu-container");
 
+menuToggle.classList.add("page-header__menu-toggle--closed");
+menuContainer.classList.add("page-header__main-menu-container--closed");
+
 menuToggle.addEventListener("click", (evt) => {
-  if (!menuToggle.classList.contains("page-header__menu-toggle--active")) {
-    menuToggle.classList.add("page-header__menu-toggle--active");
-    menuContainer.classList.add("page-header__main-menu-container--active");
-  } else if (menuToggle.classList.contains("page-header__menu-toggle--active")) {
-    menuToggle.classList.remove("page-header__menu-toggle--active");
-    menuContainer.classList.remove("page-header__main-menu-container--active");
+  let isOpened = menuToggle.classList.contains("page-header__menu-toggle--opened");
+  if (!isOpened) {
+    menuToggle.classList.remove("page-header__menu-toggle--closed");
+    menuContainer.classList.remove("page-header__main-menu-container--closed");
+    menuToggle.classList.add("page-header__menu-toggle--opened");
+    menuContainer.classList.add("page-header__main-menu-container--opened");
+  } else if (isOpened) {
+    // menuToggle.classList.remove("page-header__menu-toggle--opened");
+    // menuContainer.classList.remove("page-header__main-menu-container--opened");
+
+    menuToggle.classList.remove("page-header__menu-toggle--opened");
+    menuContainer.classList.remove("page-header__main-menu-container--opened");
+    menuToggle.classList.add("page-header__menu-toggle--closed");
+    menuContainer.classList.add("page-header__main-menu-container--closed");
   }
 });
